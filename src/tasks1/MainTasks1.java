d289545ddc7ad49a12dad7381a27d964a51e82cd
Igor Gembaruk <i.gembaruk@mail.ru>
2024-08-08 21:50:20 +0300

    new MapTutorial.class

--- src/tasks1/MainTasks1.java
+++ src/tasks1/MainTasks1.java
@@ -8,7 +8,12 @@ public class MainTasks1 {
     public static void main(String[] args) {
         MainTasks1 mainTasks1 = new MainTasks1();
         List<Integer> numbersList1 = new ArrayList<>(Arrays.asList(7, 8 , 9, 9, 9, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 3, 2, 1, 4, 6, 100, 101, 102, 104, 80, 82, 74, 73, 71, 60, 1000));
+        List<Integer> numbersList = new ArrayList<>(Arrays.asList(7, 8));
         List<String> stringList = new ArrayList<>(Arrays.asList("a", "ab", "abc", "abcde", "abcd", "a"));
+
+        System.out.println(mainTasks1.maxNumbers(numbersList));
+        System.out.println(mainTasks1.sumSquared(numbersList));
+        System.out.println(mainTasks1.countDistinctElement(stringList));
         System.out.println(mainTasks1.sortedCountLength(stringList));
         System.out.println(mainTasks1.sortedArrays(numbersList1));
         System.out.println(mainTasks1.evenArrays(numbersList1));
@@ -44,6 +49,38 @@ public class MainTasks1 {
         return sortedCountLength;
     }
 
+    //подсчет уникальных значений при помощи collect и Collectors.toSet()
+    public Integer countDistinctElement(List<String> list){
+        Set<String> countDistinctElement = list.stream()
+                .collect(Collectors.toSet());
+        return countDistinctElement.size();
+    }
+    //подсчет уникальных значений через конструктор
+    public Integer countDistinctElement2(List<String> list){
+        Set<String> countDistinctElement = new HashSet<>(list);
+        return countDistinctElement.size();
+    }
+    //сумма квадратов чисел
+    public Integer sumSquared(List<Integer>list){
+        Integer sum = list.stream()
+                .mapToInt(x -> x * x)
+                .sum();
+        return sum;
+    }
+
+    //поиск максимального числа
+    public Integer maxNumbers(List<Integer> list){
+        Optional<Integer> maxNumber = list.stream()
+                .max(Comparator.naturalOrder());
+        return maxNumber.orElse(null);
+    }
+
+    //группировка строк
+//    public Map<Integer, String> groupCountCharList(List<String> list){
+//        list.stream()
+//                .map(String::length)
+//        return
+//    }
 
 
 }
---
+++ src/tasks1/MapTutorial.java
@@ -0,0 +1,31 @@
+package tasks1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MapTutorial {
+    String str1 = "1";
+    String str2 = "2";
+    String str3 = "3";
+    String str4 = "Один";
+    String str5 = "два";
+    String str6 = "пять";
+    String str7 = "шесть";
+    String str8 = "шесть";
+    List<String> listStr1 = new ArrayList<>(Arrays.asList(str1, str2, str3, str4, str5, str6, str7, str8));
+
+    Integer number1 = 1;
+    Integer number2 = 2;
+    Integer number3 = 3;
+    Integer number4 = 4;
+    Integer number5 = 5;
+    Integer number6 = 6;
+    Integer number7 = 7;
+    Integer number8 = 1;
+    List<Integer> listNumbers2 = new ArrayList<>(Arrays.asList(number1, number2, number3, number4, number5, number6, number7, number8));
+
+
+
+
+}
